c6b367acc7fb44c1a360911d3839e92f04994270
Young, Justin <justin.kai.young@gmail.com>
2018-06-04 09:57:30 -0500

    UI update

--- swoosh/swoosh/Controller/LeagueVC.swift
+++ swoosh/swoosh/Controller/LeagueVC.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
+    @IBOutlet weak var nextBtn: BorderButton!
+    @IBOutlet weak var mensBtn: BorderButton!
+    @IBOutlet weak var womensBtn: BorderButton!
+    @IBOutlet weak var coedBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        player = Player()
         // Do any additional setup after loading the view.
     }
     
@@ -20,16 +27,38 @@ class LeagueVC: UIViewController {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+        mensBtn.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
     }
-    */
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+        womensBtn.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
+    }
+    
+    @IBAction func onCoEdTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+        coedBtn.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
+    }
+    
+    func selectLeague(leagueType: String){
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+        resetBtnColor()
+    }
+    
+    func resetBtnColor(){
+        mensBtn.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5)
+        womensBtn.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5)
+        coedBtn.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC{
+            skillVC.player = player
+        }
+    }
+
 
 }
---
+++ swoosh/swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  swoosh
+//
+//  Created by Young, Justin on 6/1/18.
+//  Copyright © 2018 Young, Justin. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+    
+}
