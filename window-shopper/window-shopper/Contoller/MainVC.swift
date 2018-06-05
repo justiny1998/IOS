6f5d81424a2ff07b215fb720730782a162969aa8
Young, Justin <justin.kai.young@gmail.com>
2018-06-05 16:28:55 -0500

    init commit

---
+++ window-shopper/window-shopper/Contoller/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Young, Justin on 6/4/18.
+//  Copyright © 2018 Young, Justin. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ window-shopper/window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,28 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Young, Justin on 6/5/18.
+//  Copyright © 2018 Young, Justin. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2455985915)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if placeholder == nil {
+            placeholder = " "
+        }
+        
+        let place = NSAttributedString(string: placeholder!,attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        attributedPlaceholder = place
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+
+}
